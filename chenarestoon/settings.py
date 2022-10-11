562019496e755a09172d92e9145990573762df96
mmdchnar <115184760+mmdchnar@users.noreply.github.com>
2022-10-11 16:50:16 +0330

    add heroku to allowed hosts

--- chenarestoon/settings.py
+++ chenarestoon/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-q3!j4ed1kd#n&xju0)pegs2^&^3!b$n_w9_&6_6^4ya%w-bo(-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chenarestoon.herokuapp.com']
 
 
 # Application definition
