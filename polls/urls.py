763432cbd81a83a99e633dfd5c6429bcf309a90e
mmdchnar <mmdchnar@gmail.com>
2022-10-13 23:31:23 +0330

    add templates

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
         path('', views.index),
+        path('<int:question_id>/', views.detail),
+        path('<int:question_id>/results/', views.results),
+        path('<int:question_id>/vote/', views.vote),
 ]
 
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from .models import Question
+
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(req):
-    return HttpResponse('''
-<p><b>Welcome to polls!</b></p>
-<p><a href="/">Go to index!</a></p>''')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+
+    return HttpResponse(template.render(context, req))
+
+
+def detail(req, question_id):
+    return HttpResponse(f'You\'re looking at question {question_id}')
+
+
+def results(req, question_id):
+    return HttpResponse(f'You\'re looking at the results of question {question_id}')
+
+
+def vote(req, question_id):
+    return HttpResponse(f'You\'re voting on question {question_id} ')
 
