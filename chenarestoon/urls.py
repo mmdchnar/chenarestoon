9022248d8313c14e5113d4d52fbfc97ce0c03e4c
mmdchnar <mmdchnar@gmail.com>
2022-11-07 11:34:17 +0330

    set 404 and 500 handler

--- chenarestoon/urls.py
+++ chenarestoon/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import handler404, handler500
 from django.urls import path, include
 from . import views
 from polls.views import files
+
+handler404 = 'polls.views.page_not_found'
+handler500 = 'polls.views.server_error'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
